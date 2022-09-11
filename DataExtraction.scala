c3219e3b3f6b457fbabf3aa8db9331ff44361e79
mrmikopi <mikailkkulak@hotmail.com>
2022-09-11 19:09:30 +0300

    Trve Generator

--- DataExtraction.scala
+++ DataExtraction.scala
@@ -1,5 +1,5 @@
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions.{monotonically_increasing_id, row_number}
+import org.apache.spark.sql.functions.{monotonically_increasing_id, row_number, rand}
 import org.apache.spark.sql.types.{StructType, StructField, StringType};
 import org.apache.spark.sql.{Row, DataFrame};
 import java.io._;
@@ -77,16 +77,57 @@ object DataExtraction {
     // val dfWords = df.select(split(col("band_names"),"[. ,-_;:/\\]").as("words")).drop("band_name")
 
     // temp view
-    df.createGlobalTempView("bands")
+    //df.createGlobalTempView("bands")
 
     // Index column for dfWords
-    dfWords = dfWords.withColumn("row_index", 
-      row_number().over(
-          Window.orderBy(monotonically_increasing_id())))
+    // dfWords = dfWords.withColumn("row_index", 
+    //   row_number().over(
+    //       Window.orderBy(monotonically_increasing_id())))
 
     // TODO t r v e olan kolonlari filtrele
     //var tempDf = df.filter(df("band_name").startsWith()).show(false)
 
     return keywords;
+  }
+
+  def getRandomTrve() : DataFrame = {
+    
+    // TODO Buraya dfT'leri olusturdugun scripti ekleyebilirsin belki
+    
+    val windowSpec = Window.partitionBy("word").orderBy("word")
+    
+    // TODO Asagidaki path'ler degismeli.
+    var dfT = spark.read.csv("/home/kaan/Repos/TRVE/deneme/dfT.csv").withColumnRenamed("_c0","word")
+    var dfR = spark.read.csv("/home/kaan/Repos/TRVE/deneme/dfR.csv").withColumnRenamed("_c0","word")
+    var dfV = spark.read.csv("/home/kaan/Repos/TRVE/deneme/dfV.csv").withColumnRenamed("_c0","word")
+    var dfE = spark.read.csv("/home/kaan/Repos/TRVE/deneme/dfE.csv").withColumnRenamed("_c0","word")
+    
+    dfT = dfT.orderBy(rand()).limit(1)    
+    dfR = dfR.orderBy(rand()).limit(1)    
+    dfV = dfV.orderBy(rand()).limit(1)    
+    dfE = dfE.orderBy(rand()).limit(1)    
+
+    // dfT = dfT.sample(1.0/dfT.count()).limit(1)
+    // dfR = dfR.sample(1.0/dfR.count()).limit(1)
+    // dfV = dfV.sample(1.0/dfV.count()).limit(1)
+    // dfE = dfE.sample(1.0/dfE.count()).limit(1)
+
+    dfT = dfT.withColumn("row_index", lit(1))
+    dfR = dfR.withColumn("row_index", lit(1))
+    dfV = dfV.withColumn("row_index", lit(1))
+    dfE = dfE.withColumn("row_index", lit(1))
+
+    dfT.show()
+    dfR.show()
+    dfV.show()
+    dfE.show()
+
+    dfT = dfT.join(dfR,dfT("row_index") === dfR("row_index"),"inner" )
+      .join(dfV,dfT("row_index") === dfV("row_index"),"inner")
+      .join(dfE,dfT("row_index") === dfE("row_index"),"inner")
+              
+    dfT.show()
+    return dfT
+
   }
 }
